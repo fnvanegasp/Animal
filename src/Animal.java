7bbcb54bf175a68c10aa3afc5cf386d24b61d391
Estudiante <Estudiante@FI454402-34.unal.loc>
2016-10-18 16:00:18 -0500

    Animal - Herencia

---
+++ src/Animal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public abstract class Animal {
+    private int numLegs;
+
+    public int getNumLegs() {
+        return this.numLegs;
+    }
+    
+    public abstract String noise();
+}
+
+
